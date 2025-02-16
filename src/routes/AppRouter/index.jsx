eb42554dc601c21fb6cdf9839a98d5515a1f4428
Stoble <stoble5281@mail.ru>
2025-02-16 20:04:51 +0300

    improve: get baseurl from env for routing

--- src/routes/AppRouter/index.jsx
+++ src/routes/AppRouter/index.jsx
@@ -2,17 +2,17 @@ import {
   Route,
   createBrowserRouter,
   createRoutesFromElements,
-} from 'react-router-dom';
-import MainLayout from '@comp/Layout';
-import { authRoutes, routes, userLayoutRoutes } from '../routes';
-import { Login, User } from '../../pages';
-import PrivateRoutes from './PrivateRoutes';
-import UserLayout from '@comp/Layout/UserLayout';
+} from "react-router-dom";
+import MainLayout from "@comp/Layout";
+import UserLayout from "@comp/Layout/UserLayout";
+import { authRoutes, routes, userLayoutRoutes } from "../routes";
+import { Login, User } from "../../pages";
+import PrivateRoutes from "./PrivateRoutes";
 
 const AppRouter = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/'>
-      <Route path='/auth' element={<Login />} end />
+    <Route path={process.env.VITE_BASENAME}>
+      <Route path="/auth" element={<Login />} end />
       <Route element={<MainLayout />}>
         {routes.map(({ path, Component }) => (
           <Route key={path} path={path} element={<Component />} end />
@@ -28,8 +28,8 @@ const AppRouter = createBrowserRouter(
           ))}
         </Route>
       </Route>
-    </Route>,
-  ),
+    </Route>
+  )
 );
 
 export default AppRouter;
