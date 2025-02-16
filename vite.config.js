6f755267624c018d14732bc5a0470a5f708c1999
Stoble <stoble5281@mail.ru>
2025-02-16 19:56:19 +0300

    fix: change vite.config.js and package.json scripts

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
 export default defineConfig({
+  base: "/react-skating-store/",
   resolve: {
     alias: {
-      '@': path.resolve(dirname, 'src'),
-      '@comp': path.resolve(dirname, 'src/components'),
-      '@hooks': path.resolve(dirname, 'src/hooks'),
-      '@assets': path.resolve(dirname, 'src/assets'),
-      '@styles': path.resolve(dirname, 'src/assets/styles'),
-      '@thunks': path.resolve(dirname, 'src/redux/thunks'),
-      '@services': path.resolve(dirname, 'src/services'),
-      '@configs': path.resolve(dirname, 'src/configs'),
+      "@": path.resolve(dirname, "src"),
+      "@comp": path.resolve(dirname, "src/components"),
+      "@hooks": path.resolve(dirname, "src/hooks"),
+      "@assets": path.resolve(dirname, "src/assets"),
+      "@styles": path.resolve(dirname, "src/assets/styles"),
+      "@thunks": path.resolve(dirname, "src/redux/thunks"),
+      "@services": path.resolve(dirname, "src/services"),
+      "@configs": path.resolve(dirname, "src/configs"),
     },
   },
   define: {
-    'process.env': {},
+    "process.env": {},
   },
   server: {
     host: true,
