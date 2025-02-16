d8751c235c11a55fc2f1f39fa50d25993ab13558
Stoble <stoble5281@mail.ru>
2025-02-16 20:14:54 +0300

    chore: delete code for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
 export default defineConfig({
-  base: "/react-skating-store/",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(dirname, "src"),
